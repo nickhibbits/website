72f1488e94ea77943c0e8aac13c871ae15379bec
Nicholas Hibbits <nicholashibbits@gmail.com>
2022-01-05 15:39:25 -0800

    refactor to create better anchor hover effect

--- src/client/app.js
+++ src/client/app.js
@@ -1,18 +1,3 @@
-export function styleSocialHover(icons) {
-  console.log("here");
-
-  icons.forEach((icon) => {
-    icon.addEventListener("mouseover", () => {
-      icon.setAttribute("color", "danger");
-    });
-  });
-  icons.forEach((icon) => {
-    icon.addEventListener("mouseout", () => {
-      icon.setAttribute("color", "dark");
-    });
-  });
-}
-
 export function makeSticky(navbar) {
   console.log("navbar", navbar);
   const sticky = navbar.offsetTop;
--- src/index.js
+++ src/index.js
@@ -3,23 +3,6 @@ import "./client/styles/body.scss";
 import "./client/styles/mediaQueries.scss";
 import "./client/styles/projects.scss";
 
-import { makeSticky, styleSocialHover } from "./client/app";
+import { makeSticky } from "./client/app";
 
-const instaIcon = document.getElementById("instagram");
-const linkdeinIcon = document.getElementById("linkedin");
-const githubIcon = document.getElementById("github");
-const emailIcon = document.getElementById("email");
-
-const icons = [instaIcon, linkdeinIcon, githubIcon, emailIcon];
-
-console.log("icons", icons);
-
-const header = document.getElementById("header");
-
-styleSocialHover(icons);
-window.onscroll = function () {
-  makeSticky(header);
-};
-
-export { styleSocialHover };
 export { makeSticky };
