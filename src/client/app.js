853ff8b464e8e79b658190328d4c3c50c89c2868
Nicholas Hibbits <nicholashibbits@gmail.com>
2022-01-07 20:43:07 -0800

    scroll to section from nav link

--- src/client/app.js
+++ src/client/app.js
@@ -12,16 +12,16 @@ export function scrollToSection() {
   console.log("here");
   let sections = document.querySelectorAll("section");
   console.log("sections", sections);
-  sections = Array.from(sections).map((section) => section.id);
-  let sectionIds = sections.filter(
-    (section) => section === "section1" || "section2" || "section3"
-  );
+  let sectionIds = Array.from(sections).map((section) => section.id);
   console.log("sectionIds", sectionIds);
 
-  document.querySelectorAll("li").forEach((navItem) => {
+  Array.from(document.querySelector("ul").children).forEach((navItem) => {
     navItem.addEventListener("click", (e) => {
       e.preventDefault();
-      document.querySelector;
+      document.querySelector(navItem.getAttribute("href")).scrollIntoView({
+        block: "center",
+        behavior: "smooth",
+      });
     });
   });
 }
